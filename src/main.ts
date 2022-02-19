a95f7ae563012d86bf46f19de9291782450e85c7
Peter van der Noord <peterdunord@gmail.com>
2022-02-19 01:32:00 +0100

    Simple bot setup that replies to messages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-const main = () => {
-  console.log('main');
+import 'dotenv/config';
+import { Client, Intents } from 'discord.js';
+
+const connectBot = () => {
+  const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+  client.once('ready', () => {
+    console.log('Ready!');
+  });
+
+  client.on('message', (message) => {
+    if (message.content.startsWith('!')) {
+      message.reply(`Replying to ${message.author.username}`);
+    }
+  });
+
+  client.login(process.env.BOT_TOKEN);
 };
 
-main();
+connectBot();
